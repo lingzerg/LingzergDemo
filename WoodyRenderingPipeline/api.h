a458dca5b823e5a30418d5b08bb7bbc36f92630c
lingzerg <lingzerg@qq.com>
2022-01-22 13:36:18 +0800

    提交练习项目

---
+++ WoodyRenderingPipeline/api.h
@@ -0,0 +1,7 @@
+#ifndef API_H
+#define API_H
+
+//#include "platform.h"
+#include "macro.h"
+
+#endif
---
+++ WoodyRenderingPipeline/win32.cpp
@@ -0,0 +1,13 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <direct.h>
+#include <windows.h>
+
+#include "platform.h"
+#include "macro.h"
+
+//
+//window_t* window_create(const char* title, int width, int height) {
+//
+//}
