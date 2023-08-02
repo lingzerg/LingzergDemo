d41dcf972682e0d477a3eee3e7757c9977f7e7e8
lingzerg <lingzerg@qq.com>
2023-08-02 16:31:48 +0800

    提交一个软光追代码用于学习记录

---
+++ MyRendering/MyRendering.cpp
@@ -0,0 +1,91 @@
+// MyRendering.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include "rtweekend.h"
+
+struct Pixel {
+    int r;
+    int g;
+    int b;
+};
+
+#include "color.h"
+#include "hittable_list.h"
+#include "sphere.h"
+#include "camera.h"
+
+#include <iostream>
+#include <fstream>
+
+color ray_color(const ray& r, const hittable& world, int depth) {
+    hit_record rec;
+
+    // If we've exceeded the ray bounce limit, no more light is gathered.
+    if (depth <= 0)
+        return color(0, 0, 0);
+
+    if (world.hit(r, 0, infinity, rec)) {
+        point3 target = rec.p + rec.normal + random_in_unit_sphere();
+        return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth - 1);
+    }
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
+
+
+std::vector<Pixel> pixelList;
+
+int main() {
+    
+    // Image
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 100;
+    const int max_depth = 50;
+
+    // World
+    hittable_list world;
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+
+    // Camera
+    camera cam;
+    
+    // Render
+    //std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = image_height - 1; j >= 0; --j) {
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+        for (int i = 0; i < image_width; ++i) {
+            color pixel_color(0, 0, 0);
+            for (int s = 0; s < samples_per_pixel; ++s) {
+                auto u = (i + random_double()) / (image_width - 1);
+                auto v = (j + random_double()) / (image_height - 1);
+                ray r = cam.get_ray(u, v);
+                pixel_color += ray_color(r, world, max_depth);
+            }
+            pixelList.push_back(write_color(std::cout, pixel_color, samples_per_pixel));
+        }
+    }
+
+    // 打开一个文件以写入数据
+    std::ofstream outfile("output.ppm", std::ios::out);
+
+    // 检查文件是否成功打开
+    if (!outfile.is_open()) {
+        std::cerr << "Error: Unable to open the file." << "\n";
+        return 1;
+    }
+    outfile << "P3\n" << '\n' << image_width << ' ' << image_height << "\n255\n";
+    
+    // 遍历std::vector，并将每个Person结构的数据写入文件
+    for (const Pixel& p : pixelList) {
+        outfile << p.r << ' ' << p.g << ' ' << p.b << "\n";
+    }
+
+    // 关闭文件输出流
+    outfile.close();
+
+    std::cerr << "\nDone.\n";
+}
---
+++ MyRendering/run.bat
@@ -0,0 +1,1 @@
+Debug\MyRendering.exe > image.ppm
